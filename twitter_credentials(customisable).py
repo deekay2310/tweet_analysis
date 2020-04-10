c646b9b81816472cd86c4ecdf9c5c9eb747c5ad2
Dev Kumar <devskumar99@gmail.com>
2020-04-10 13:20:53 +0530

    Add files via upload

---
+++ twitter_credentials(customisable).py
@@ -0,0 +1,13 @@
+'''
+Variables for user credentials to access twitter API
+Rename this file to twitter_credentials. 
+Go to developer.twitter.com to generate the required tokens and paste them here. 
+''' 
+
+#access tokens
+access_token = "paste your token here"
+access_token_secret = "paste your token here"
+
+#consumer tokens
+consumer_key = "paste your token here"
+consumer_key_secret = "paste your token here"
